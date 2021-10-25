d7b670a7a3e70e953c90a6304262dcbb79e7df2f
ottostephen88 <83191352+ottostephen88@users.noreply.github.com>
2021-10-24 21:10:52 -0600

    Add files via upload

---
+++ code/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿
+namespace SoftRobotics
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox_comPort = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button_close = new System.Windows.Forms.Button();
+            this.button_open = new System.Windows.Forms.Button();
+            this.progressBar_statusBar = new System.Windows.Forms.ProgressBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox_baudRate = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_sent = new System.Windows.Forms.TextBox();
+            this.richTextBox_textReceiver = new System.Windows.Forms.RichTextBox();
+            this.button_send = new System.Windows.Forms.Button();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(-552, -72);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(400, 200);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "COM Settings";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "COM Port";
+
+            // 
+            // comboBox_comPort
+            // 
+            this.comboBox_comPort.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_comPort.FormattingEnabled = true;
+            this.comboBox_comPort.Location = new System.Drawing.Point(25, 122);
+            this.comboBox_comPort.Name = "comboBox_comPort";
+            this.comboBox_comPort.Size = new System.Drawing.Size(242, 40);
+            this.comboBox_comPort.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button_close);
+            this.groupBox2.Controls.Add(this.button_open);
+            this.groupBox2.Controls.Add(this.progressBar_statusBar);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.comboBox_baudRate);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.comboBox_comPort);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(33, 33);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(430, 650);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "COM Settings";
+            // 
+            // button_close
+            // 
+            this.button_close.Location = new System.Drawing.Point(223, 492);
+            this.button_close.Name = "button_close";
+            this.button_close.Size = new System.Drawing.Size(150, 49);
+            this.button_close.TabIndex = 7;
+            this.button_close.Text = "Close";
+            this.button_close.UseVisualStyleBackColor = true;
+            // 
+            // button_open
+            // 
+            this.button_open.Location = new System.Drawing.Point(25, 495);
+            this.button_open.Name = "button_open";
+            this.button_open.Size = new System.Drawing.Size(150, 46);
+            this.button_open.TabIndex = 6;
+            this.button_open.Text = "Open";
+            this.button_open.UseVisualStyleBackColor = true;
+            // 
+            // progressBar_statusBar
+            // 
+            this.progressBar_statusBar.Location = new System.Drawing.Point(39, 374);
+            this.progressBar_statusBar.Name = "progressBar_statusBar";
+            this.progressBar_statusBar.Size = new System.Drawing.Size(200, 46);
+            this.progressBar_statusBar.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 320);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 32);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Status";
+            // 
+            // comboBox_baudRate
+            // 
+            this.comboBox_baudRate.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_baudRate.FormattingEnabled = true;
+            this.comboBox_baudRate.Items.AddRange(new object[] {
+            "9600",
+            "38400",
+            "115200"});
+            this.comboBox_baudRate.Location = new System.Drawing.Point(34, 243);
+            this.comboBox_baudRate.Name = "comboBox_baudRate";
+            this.comboBox_baudRate.Size = new System.Drawing.Size(233, 40);
+            this.comboBox_baudRate.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 178);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 32);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Baud Rate";
+            // 
+            // textBox_sent
+            // 
+            this.textBox_sent.Location = new System.Drawing.Point(641, 60);
+            this.textBox_sent.Name = "textBox_sent";
+            this.textBox_sent.Size = new System.Drawing.Size(200, 39);
+            this.textBox_sent.TabIndex = 4;
+            // 
+            // richTextBox_textReceiver
+            // 
+            this.richTextBox_textReceiver.Location = new System.Drawing.Point(682, 230);
+            this.richTextBox_textReceiver.Name = "richTextBox_textReceiver";
+            this.richTextBox_textReceiver.Size = new System.Drawing.Size(200, 192);
+            this.richTextBox_textReceiver.TabIndex = 5;
+            this.richTextBox_textReceiver.Text = "";
+            // 
+            // button_send
+            // 
+            this.button_send.Location = new System.Drawing.Point(1023, 53);
+            this.button_send.Name = "button_send";
+            this.button_send.Size = new System.Drawing.Size(150, 46);
+            this.button_send.TabIndex = 6;
+            this.button_send.Text = "Send";
+            this.button_send.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1224, 793);
+            this.Controls.Add(this.button_send);
+            this.Controls.Add(this.richTextBox_textReceiver);
+            this.Controls.Add(this.textBox_sent);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox_comPort;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button_close;
+        private System.Windows.Forms.Button button_open;
+        private System.Windows.Forms.ProgressBar progressBar_statusBar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox_baudRate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_sent;
+        private System.Windows.Forms.RichTextBox richTextBox_textReceiver;
+        private System.Windows.Forms.Button button_send;
+    }
+}
+
